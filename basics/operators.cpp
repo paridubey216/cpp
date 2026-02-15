4154b43ddd8a4daab82d902796bd42113d660689
pari <paridubey216@gmail.com>
2026-02-15 22:13:25 +0530

    Create operators.cpp with operator documentation
    Added documentation on operator types in C++.

---
+++ basics/operators.cpp
@@ -0,0 +1,12 @@
+//Operators are used to perform operations on variables and values
+/*C++ divides the operators into the following groups:
+1.Arithmetic operators
+2.Assignment operators
+3.Comparison operators
+4.Logical operators
+5.Bitwise operators*/
+
+
+/* 1)Arithmetic Operators
+Arithmetic operators are used to perform common mathematical operations*/
+
